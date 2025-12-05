435ffab6af41f21c8c79012e2160048596739ae0
mok <99533961+parcPrive@users.noreply.github.com>
2025-12-05 17:37:15 +0900

    어드민_유저상세기능_1

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-const PROXY_URL = import.meta.env.VITE_PROXY_URL;
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, import.meta.dirname, "");
 
-export default defineConfig({
+  return {
     plugins: [react(), tailwindcss()],
     server: {
-        proxy: {
-            "^/(api|analysis|users)": {
-                target: PROXY_URL,
-                changeOrigin: true,
-                secure: false
-            }
+      proxy: {
+        "^/(api|analysis|users)": {
+          target: env.VITE_PROXY_URL,
+          changeOrigin: true,
+          secure: false,
         },
-        host: true,
-        port: 5173
-    }
-})
+      },
+      host: true,
+      port: 5173,
+    },
+  };
+});
