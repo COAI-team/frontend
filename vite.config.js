79c18f836ea04939e20afbbd15835e7ce8c69847
MinseokChoi <seok7521@gmail.com>
2025-12-10 22:04:43 +0900

    https 적용

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import fs from "node:fs";
+import path from "node:path";
 
+/* eslint-env node */
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, import.meta.dirname, '');
 
     return {
         plugins: [react(), tailwindcss()],
         server: {
+            https: {
+                key: fs.readFileSync(path.resolve(import.meta.dirname, "localhost-key.pem")),
+                cert: fs.readFileSync(path.resolve(import.meta.dirname, "localhost-cert.pem")),
+            },
             proxy: {
                 "^/(api|analysis|users)": {
-                    target: env.VITE_PROXY_URL || 'http://localhost:9443',
+                    target: env.VITE_PROXY_URL || "http://localhost:9443",
                     changeOrigin: true,
                     secure: false
                 }
@@ -19,4 +26,4 @@ export default defineConfig(({ mode }) => {
             port: 5173
         }
     };
-})
+});
