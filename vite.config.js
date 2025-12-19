ab2389218f4712b28672eddde2fe4eedab88163b
MinseokChoi <seok7521@gmail.com>
2025-12-19 15:32:05 +0900

    코드 수정

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
+/* eslint-env node */
+/* global process */
+
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import monacoEditorPlugin from "vite-plugin-monaco-editor";
 import fs from "node:fs";
 import path from "node:path";
-import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss.d.ts";
 
-/* eslint-env node */
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
@@ -28,14 +29,14 @@ export default defineConfig(({ mode }) => {
       global: "window"
     },
     server: {
-      // https: {
-      //   key: fs.readFileSync(
-      //     path.resolve(process.cwd(), "localhost-key.pem")
-      //   ),
-      //   cert: fs.readFileSync(
-      //     path.resolve(process.cwd(), "localhost-cert.pem")
-      //   )
-      // },
+      https: {
+        key: fs.readFileSync(
+          path.resolve(process.cwd(), "localhost-key.pem")
+        ),
+        cert: fs.readFileSync(
+          path.resolve(process.cwd(), "localhost-cert.pem")
+        )
+      },
       proxy: {
         "^/(api|analysis|users)": {
           target: env.VITE_PROXY_URL || "http://localhost:9443",
