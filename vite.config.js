0ce548013217802ff58d73cfa8a97cdfdc8d854e
MinseokChoi <seok7521@gmail.com>
2025-12-19 15:21:26 +0900

    코드 수정

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,50 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import monacoEditorPlugin from "vite-plugin-monaco-editor";
 import fs from "node:fs";
 import path from "node:path";
+import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss.d.ts";
 
 /* eslint-env node */
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, import.meta.dirname, '');
+  const env = loadEnv(mode, process.cwd(), "");
 
-    return {
-        plugins: [react(), tailwindcss()],
-        define: {
-            global: 'window'
-        },
-        server: {
-            // https: {
-            //     key: fs.readFileSync(path.resolve(import.meta.dirname, "localhost-key.pem")),
-            //     cert: fs.readFileSync(path.resolve(import.meta.dirname, "localhost-cert.pem")),
-            // },
-            proxy: {
-                "^/(api|analysis|users)": {
-                    target: env.VITE_PROXY_URL || "http://localhost:9443",
-                    changeOrigin: true,
-                    secure: false
-                }
-            },
-            host: true,
-            port: 5173
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      monacoEditorPlugin({
+        languageWorkers: [
+          "json",
+          "typescript",
+          "javascript",
+          "css",
+          "html"
+        ]
+      })
+    ],
+    define: {
+      global: "window"
+    },
+    server: {
+      // https: {
+      //   key: fs.readFileSync(
+      //     path.resolve(process.cwd(), "localhost-key.pem")
+      //   ),
+      //   cert: fs.readFileSync(
+      //     path.resolve(process.cwd(), "localhost-cert.pem")
+      //   )
+      // },
+      proxy: {
+        "^/(api|analysis|users)": {
+          target: env.VITE_PROXY_URL || "http://localhost:9443",
+          changeOrigin: true,
+          secure: false
         }
-    };
+      },
+      host: true,
+      port: 5173
+    }
+  };
 });
